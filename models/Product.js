f7140480fad0d5dc656f2540eac73b0231a8e9f1
naseef786 <naseefac786@gmail.com>
2023-10-19 04:02:50 +0530

    updated backend routed

--- models/Product.js
+++ models/Product.js
@@ -1,21 +1,38 @@
-const { default: mongoose } = require("mongoose");
+// models/index.js
+
+const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-  description: String,
+  description: {
+    type: String,
+    required: true,
+  },
   price: {
-    type: Number,
+    type: String,
+    required: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category', // Assuming you have a Category model
     required: true,
   },
   subcategory: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Subcategory'
-  }
+    ref: 'Subcategory', // Assuming you have a Subcategory model
+    required: true,
+  },
+  endCategory: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Subcategory', // Assuming you have a model for end categories
+    required: true,
+  },
 });
 
 const Product = mongoose.model('Product', productSchema);
 
-module.exports  = {Product}
+module.exports =  Product ;
+
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,23 +1,29 @@
 const express = require('express');
-const Product = require('../models/product');
-const { Subcategory } = require('../models/Category');
+const Product = require('../models/Product');
+const { Subcategory, Category } = require('../models/Category');
 
 const router = express.Router();
 
 router.post('/products', async (req, res) => {
   try {
-    const { name, description, price, subcategoryId } = req.body;
-    const subcategory = await Subcategory.findById(subcategoryId);
+    console.log(req.body);
+    const { name, description, price, category, subcategoryId, endCategory } = req.body;
 
-    if (!subcategory) {
-      return res.status(404).json({ message: 'Subcategory not found' });
+    const subcategory = await Subcategory.findOne({ name: subcategoryId });
+    const mainCategory = await Category.findOne({ name: category });
+    const endSub = await Subcategory.findOne({ name: endCategory });
+
+    if (!subcategory || !mainCategory || !endSub) {
+      return res.status(404).json({ message: 'One or more categories not found' });
     }
 
     const product = await Product.create({
       name,
       description,
       price,
-      subcategory: subcategory._id
+      subcategory: subcategory._id,
+      category: mainCategory.id,
+      endCategory: endSub.id
     });
 
     res.status(201).json({ message: 'Product created successfully', product });
@@ -25,6 +31,20 @@ router.post('/products', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+router.get('/products', async (req, res) => {
+  try {
+  
+console.log("inside products");
+   
+const categories = await Category.find().populate('subcategories');
+const subcategories = await Subcategory.find().populate('subcategories');
+const products = await Product.find().exec()
+res.status(200).json({ categories, subcategories,products });
+   
 
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
